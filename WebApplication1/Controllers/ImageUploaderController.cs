c5222e5ee08fb0436b2b639e78679122170f24e4
himanshu2454 <hchouhan3654@gmail.com>
2021-03-01 03:42:19 +0530

    Initial commit

---
+++ WebApplication1/Controllers/ImageUploaderController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ImageUploader.Models;
+using Newtonsoft.Json;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class ImageUploaderController : ControllerBase
+    {
+        
+       
+            private readonly IWebHostEnvironment env;
+
+            public ImageUploaderController(IWebHostEnvironment environment)
+            {
+                env = environment ?? throw new ArgumentNullException(nameof(environment));
+            }
+
+            // POST: /image
+            [HttpPost]
+            public async Task Post(IFormFile file)
+            {
+                var uploads = Path.Combine(env.WebRootPath, "uploads");
+                if (file.Length > 0)
+                {
+                    using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream);
+                    }
+                }
+            }
+
+            [HttpGet]
+            public List<string> Get()
+            {
+                 var imagelist = new List<string>();
+                //Fetch Uploaded Image Data.
+                var path = Path.Combine(env.WebRootPath, "uploads");
+                DirectoryInfo info = new DirectoryInfo(path);
+                FileInfo[] files = info.GetFiles().OrderBy(p => p.CreationTime).ToArray();
+                foreach (var file in files)
+                {
+                    imagelist.Add(file.Name);
+                }
+
+                return imagelist;
+            }
+
+        [HttpGet("asc")]
+        public List<string> GetAsc()
+        {
+            var imagelist = new List<string>();
+            //Fetch Uploaded Image Data.
+            var path = Path.Combine(env.WebRootPath, "uploads");
+            DirectoryInfo info = new DirectoryInfo(path);
+            FileInfo[] files = info.GetFiles().OrderBy(p => p.CreationTime).ToArray();
+            foreach (var file in files)
+            {
+                imagelist.Add(file.Name);
+            }
+
+            return imagelist;
+        }
+
+        [HttpGet("des")]
+        public List<string> GetDsc()
+        {
+            var imagelist = new List<string>();
+            //Fetch Uploaded Image Data.
+            var path = Path.Combine(env.WebRootPath, "uploads");
+            DirectoryInfo info = new DirectoryInfo(path);
+            FileInfo[] files = info.GetFiles().OrderByDescending(p => p.CreationTime).ToArray();
+            foreach (var file in files)
+            {
+                imagelist.Add(file.Name);
+            }
+
+            return imagelist;
+        }
+
+    }
+}
---
+++ WebApplication1/UploadedImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageUploader.Models
+{
+    public class UploadedImage
+    {
+        public UploadedImage(string name)
+        {
+            this.ImageName = name;
+            
+        }
+        public string ImageName { get; set; }
+        public DateTime TimeAdded { get; set; }
+    }
+}
